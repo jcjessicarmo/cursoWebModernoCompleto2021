b6cc1bc53f92db83c65d598e47d460ddf5ef6afe
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-17 13:51:41 -0300

    Adicionando exercicio da aula 46

---
+++ exercicios-web/fundamentos/objeto.js
@@ -0,0 +1,22 @@
+//Seção 04 - Aula 46
+
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega',
+prod1.preco = 4998.90
+console.log(prod1)
+
+
+//Evitar atributos com espaços
+const prod2 = {}
+prod2['nome'] = 'Celular Ultra Mega',
+prod2['preco'] = 4998.90
+console.log(prod2)
+
+
+
+const prod3 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+console.log(prod3)
+// RESULTADO: '{ nome: 'Camisa Polo', preco: 79.9 }'
