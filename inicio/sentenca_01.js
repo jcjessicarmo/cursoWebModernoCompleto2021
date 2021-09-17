7a406b689f77d500520b2db87ca26981aba6f249
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-16 22:56:12 -0300

    Adicionando as primeiras sentenças de código

---
+++ inicio/sentenca_01.js
@@ -0,0 +1,5 @@
+//Seção 03 - Aula 16 | Sentença de código = frase(s).
+console.log('Good morning!');
+console.log('Good afternoon!');
+console.log('Good evening!');
+//console.log é usado para exibir algo na tela do computador!
---
+++ inicio/sentenca_02.js
@@ -0,0 +1,4 @@
+//Seção 03 - Aula 17
+console.log('First');
+console.log('Second');
+console.log('Third');
