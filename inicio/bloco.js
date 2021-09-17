4fd72ab533641d8673dbe64301f7d80753b1d708
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-16 22:57:54 -0300

    Adicionando comentários e exemplo de blocos

---
+++ inicio/bloco.js
@@ -0,0 +1,27 @@
+//Seção 03 - Aula 19 | Bloco de código
+// 05 Sentenças de códigos e 02 blocos de códigos
+{
+    console.log('A');
+    console.log('B');
+    console.log('C');
+}
+
+{
+    console.log('D');
+    console.log('E');
+    console.log('F');
+}
+
+{
+    //bloco vazio
+}
+
+{
+    {
+        {
+            {
+                console.log('1 sentença de códigos, dentro de 4 blocos');
+            }
+        }
+    }
+}
---
+++ inicio/comentario.js
@@ -0,0 +1,17 @@
+//Seção 03 - Aula 18
+
+console.log('Comment 1');
+//Exemplo de comentário
+
+console.log('Comment 2');
+/*
+Exemplo de comentário com várias linhas!
+*/
+
+console.log('Comment 3');
+/*
+* Outro
+* Exemplo
+* De comentário
+* Para várias linhas
+*/
