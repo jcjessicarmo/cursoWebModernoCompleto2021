bc6d2d878c4ce112035b72e7c8a08d1877bb43a5
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-16 23:00:33 -0300

    Adicionando aula 22 e 23 - trabalhando com dados

---
+++ inicio/dados_1.js
@@ -0,0 +1,8 @@
+//Seção 03 - Aula 22
+
+var nome = 'caneta';
+var quantidade = 10;
+var cor = 'preta';
+var preco = 3.00;
+var precoFinal = quantidade * preco;
+console.log(precoFinal);
---
+++ inicio/dados_2.js
@@ -0,0 +1,17 @@
+//Seção 03 - Aula 23
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1- desconto);
+console.log(precoComDesconto);
+
+//Exemplo2
+let nome = 'caderno';
+let cor = 'azul';
+console.log(nome + ' ' + cor);
+
+//Exemplo3
+let nomeCompleto = 'Ana Vitória da Silva';
+let idade = 12;
+let cidade = 'Mauá';
+console.log('Dados pessoais: ' + 'Nome:' + nomeCompleto + ' ' + 'Idade:' + idade + ' ' + 'Cidade:' + cidade);
