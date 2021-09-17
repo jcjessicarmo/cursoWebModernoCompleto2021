89b57bb31a104e738f8a0953870a42e4b50d8e5f
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-16 22:59:22 -0300

    Adicionando o desafio de sentença e bloco

---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,39 @@
+//Seção 03 - Aula 20 
+
+/*
+Desafio 01: Criar 2 blocos com 5 sentença dentro de cada bloco.
+EXEMPLO:
+
+Bloco1
+1
+3
+5
+7
+9
+
+Bloco2
+2
+4
+6
+8
+10
+*/
+
+//Bloco 1
+{
+    console.log('1');
+    console.log('3');
+    console.log('5');
+    console.log('7');
+    console.log('9');
+}
+
+//Bloco 2
+{
+    console.log('2');
+    console.log('4');
+    console.log('6');
+    console.log('8');
+    console.log('10');
+
+}
