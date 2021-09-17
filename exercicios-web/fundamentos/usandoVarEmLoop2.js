9cff0d8e556aec900dd817387b3be4954be37ce1
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-17 17:00:49 -0300

    Atualizando exercicio da aula 55

--- exercicios-web/fundamentos/usandoVarEmLoop2.js
+++ exercicios-web/fundamentos/usandoVarEmLoop2.js
@@ -1,17 +1,5 @@
-//Seção 04 - Aula 55 e 56
+//Seção 04 - Aula 55
 
 for (let i = 0; i < 10; i++) {
     console.log(i)
 }
-
-
-const funcs = []
-
-for (var i = 0; i < 10; i++) {
-    funcs.push(function() {
-        console.log(i)
-    })
-}
-
-funcs[2]()
-funcs[8]()
