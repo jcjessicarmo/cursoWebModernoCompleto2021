40b7c339b1cca2ec1fdb89e4dc2fe1948f781aed
Jéssica S. Carmo <55473356+jcjessicarmo@users.noreply.github.com>
2021-09-17 13:33:51 -0300

    Delete exercicios-web/fundamentos directory
    Deletando exercicios-web/fundamentos pois commitei a seção incorreta.

--- exercicios-web/fundamentos/numerosAlgunsCuidados.js
+++
@@ -1,8 +0,0 @@
-//Seção 04 - Aula 40
-
-console.log(7 / 0) //Infinity
-console.log("10.50"/2) //Mesmo o número estando dentro das aspas ele faz o calculo
-console.log("Show!" * 2) //Não é possível multiplicar 
-console.log(0.10 + 0.70) // imprecisão
-// console.log(10.toString()) não é possível
-console.log((10.345).toFixed(2)) //literal dentro de parentese 
