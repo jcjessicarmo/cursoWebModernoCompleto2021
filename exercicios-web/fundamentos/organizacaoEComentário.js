fc34ddecb71d2d45f2f2c4f5910c738d551b3393
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-17 12:46:58 -0300

    Criando pasta de exercícios/fundamentos e adicionando aula 34, 35 e 36

---
+++ exercicios-web/fundamentos/organizacaoEComentário.js
@@ -0,0 +1,24 @@
+//Seção 04 - Aula 34, 35 e 36
+console.log("Sentença de código")
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') //Padrão do curso - Utilizar aspas simples ('') e sem (;)
+    }
+}
+
+// Comentário de uma linha
+console.log('Linha 1')
+
+/*
+ Comentário de
+ múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * múliplas linhas
+ */
+console.log('Linha 3')
