da1a2d1715642cd4d3957df8e67ab23fa92949b2
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-17 13:43:41 -0300

    Adicionando exercicio da aula 38

---
+++ exercicios-web/fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+//Seção 04 - Aula 38
+
+//JavaScript é uma linguagem de tipagem fraca
+
+let qualquerCoisa = 'E aí, como vai?'
+console.log(qualquerCoisa)
+console.log(typeof qualquerCoisa)
+
+qualquerCoisa = 3.1234,32 
+console.log(qualquerCoisa)
+console.log(typeof qualquerCoisa)
+
+//Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto químico perigoso ... KKKK
