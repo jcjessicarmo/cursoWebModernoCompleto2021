761e3e4329037fa9dabfd9821430760fd4b5c2c4
jcjessicarmo <jessicascarmo@terra.com.br>
2021-09-17 16:05:48 -0300

    Adicionando exercicio da aula 51

---
+++ exercicios-web/fundamentos/usandoVar1.js
@@ -0,0 +1,14 @@
+//Seção 04 - Aula 51
+
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Será?' 
+                console.log(sera)
+            } 
+        } 
+    } 
+}
+
+console.log(sera)
